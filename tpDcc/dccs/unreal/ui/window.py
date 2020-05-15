26c8d6b0971490a80a70384cb27fad71d5949ca0
Tomas Poveda <tpovedatd@gmail.com>
2020-05-15 03:08:52 +0200

    Added example of tick loop for tpDc Unreal windows

--- tpDcc/dccs/unreal/ui/window.py
+++ tpDcc/dccs/unreal/ui/window.py
@@ -22,6 +22,21 @@ class UnrealWindow(core_window.MainWindow, object):
 
         if not parent:
             unreal.parent_external_window_to_slate(self.winId())
+            print(self.parent())
+
+    # NOTE: Code to create tick event for specific windows
+    #     self._tick_handle = unreal.register_slate_post_tick_callback(self._on_unreal_app_tick)
+    #     self.closed.connect(self._on_unreal_window_close)
+    #
+    # def tick(self, delta_seconds, *args, **kwargs):
+    #     print(self.WindowId, delta_seconds)
+    #
+    # def _on_unreal_app_tick(self, delta_seconds):
+    #     self.tick(delta_seconds)
+    #
+    # def _on_unreal_window_close(self):
+    #     if self._tick_handle:
+    #         unreal.unregister_slate_post_tick_callback(self._tick_handle)
 
 
 register.register_class('Window', UnrealWindow)
